57b6651a226247ea45ddd1230ba3a051b3012d40
David Bayless <David.C.Bayless15@gmail.com>
2016-02-04 14:46:40 -0700

    init commit

---
+++ pokeForceFront/js/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('TemplateApp', ['ngRoute']);
+// Change Me
---
+++ pokeForceFront/js/controllers.js
@@ -0,0 +1,7 @@
+app.controller('TemplateController', [TemplateController]);
+// Change Me
+
+function TemplateController() {
+  var vm = this;
+  vm.title = 'Angular Template'
+}
