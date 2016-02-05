c727ec2e7b869dd8ec3c004c5d2964f8ca828caa
David Bayless <David.C.Bayless15@gmail.com>
2016-02-04 17:08:00 -0700

    added test logic

--- serverPokeForce/routes/users.js
+++ serverPokeForce/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+
+function pokemon() {
+  return knex('pokemon');
+}
+
+function users() {
+  return knex('users');
+}
+
+function savegame() {
+  return knex('savegame');
+}
+
+function party() {
+  return knex('party');
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  users().select().fullOuterJoin('party', 'users.savegameidone', 'party.id')
+  .then(function(response) {
+    console.log(response);
+    party().select().where('party.id', response[0].savegameidone).fullOuterJoin('pokemon', 'party.pokeone', 'pokemon.joinid')
+    .then(function(newResponse) {
+      res.json(newResponse);
+    });
+  });
 });
 
 module.exports = router;
